7f270a71bc1f4d7d363543fa37aa6d3f57824853
Parsa Yazdani <3parsa3@gmail.com>
2021-04-13 04:24:54 +1000

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,16 @@
 <?php
-$output = shell_exec('cd ' . __DIR__ . ' && ar x file.deb && tar -xf control.tar.gz');
+$output = shell_exec('cd ' . __DIR__ . ' && ar x file.deb && tar -xf control.tar.gz && tar -xf control.tar.xz');
 unlink('debian-binary');
 unlink('data.tar.lzma');
+unlink('data.tar.xz');
 unlink('control.tar.gz');
+unlink('control.tar.xz');
+unlink('postinst');
 foreach(explode(PHP_EOL, file_get_contents('control')) as $line){
     if(str_starts_with($line, 'Package: '))
         $bundleid = substr($line, 9);
     if(str_starts_with($line, 'Name: '))
-        $packagename = substr($line, 7);
+        $packagename = substr($line, 6);
     if(str_starts_with($line, 'Depends: '))
         $depends = substr($line, 9);
     if(str_starts_with($line, 'Description: '))
